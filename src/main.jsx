938b1c0e0cb5d824132980c4f2adfda807dd20ce
Eduardo Lange <eduardoandrelange@gmail.com>
2024-03-13 21:21:15 -0300

    show de clica

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,24 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Home from './routes/Home.jsx'
+import Contact from './routes/Contact.jsx'
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+		children: [
+			{ path: '/', element: <Home /> },
+			{ path: '/contact', element: <Contact /> },
+		],
+	},
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<RouterProvider router={router}/>
+	</React.StrictMode>
 )
