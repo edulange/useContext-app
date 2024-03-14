d9ceef1cee64abf786061a27f148aed4634d68ca
Eduardo Lange <eduardoandrelange@gmail.com>
2024-03-13 22:01:31 -0300

    deu certo!

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,18 @@ import './App.css'
 import { Outlet } from 'react-router-dom'
 import Navbar from './Components/Navbar'
 
+//qq eu vou fazer? vou mudar o theme apartir do Apps
+//vou fazer uma condicional para verificar qual é o contexto
+
+//primeiro precisa consumir o contexto
+import { useContext } from 'react'
+import { ThemeContext } from './context/ThemeContext'
+
 function App() {
+  const {theme} = useContext(ThemeContext)
 	return (
-		<div className='App'>
-      <Navbar />
+		<div className={`App ${theme === 'dark' ? 'dark-theme' : ''}`}>
+			<Navbar />
 			<Outlet />
 		</div>
 	)
